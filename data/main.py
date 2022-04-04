9336c5c9d56ad7c1e3c45d8de15bc75b598c1d5f
DaleNaci <dtn22@njit.edu>
2022-04-04 09:09:28 -0400

    Separated UI into its own components folder named 'ui_components'

--- data/main.py
+++ data/main.py
@@ -1,90 +1,7 @@
 import turtle
 
 from .components.game import Game
-
-
-def draw_hex(t):
-    for i in range(6):
-        t.forward(60)
-        t.left(60)
-
-
-def row_of_hexes(t, n):
-    for i in range(n-1):
-        draw_hex(t)
-        t.up()
-        t.forward(60)
-        t.right(60)
-        t.forward(60)
-        t.left(60)
-        t.down()
-    draw_hex(t)
-
-
-def snake(t, seq):
-    for c in seq:
-        if c == "f":
-            t.forward(60)
-        elif c == "r":
-            t.right(60)
-        else:
-            t.left(60)
-
-
-def draw_board(t, game):
-    t.speed(0)
-
-    t.up()
-    t.goto(-30, 365)
-    t.down()
-
-    row_of_hexes(t, 5)
-
-    snake(t, "flll")
-    row_of_hexes(t, 6)
-
-    snake(t, "flfrflflfl")
-    row_of_hexes(t, 7)
-
-    snake(t, "frrr")
-    row_of_hexes(t, 8)
-
-    snake(t, "flfrflflfl")
-    row_of_hexes(t, 9)
-
-    snake(t, "lllrfl")
-    row_of_hexes(t, 8)
-
-    snake(t, "flflfrflfl")
-    row_of_hexes(t, 7)
-
-    snake(t, "llfl")
-    row_of_hexes(t, 6)
-
-    snake(t, "flflfrflfl")
-    row_of_hexes(t, 5)
-
-
-def draw_pieces(wn, game):
-    with open("./resources/coordinate_data/hex_coords.txt") as f:
-        for line in f.readlines():
-            q, r, s, x, y = line.split()
-
-            a = turtle.Turtle()
-            a.up()
-            a.shape("circle")
-            a.goto(float(x), float(y))
-
-            key = f"{q}, {r}, {s}"
-            tile = game.board.board[key]
-            if tile.state == 0:
-                a.color("white")
-            elif tile.state == 1:
-                a.color("red")
-            else:
-                a.color("blue")
-
-    wn.update()
+from .ui_components.turtle_functions import *
 
 
 def main():
@@ -95,21 +12,16 @@ def main():
     wn.tracer(0)
 
     t = turtle.Turtle()
-
     draw_board(t, game)
-
     draw_pieces(wn, game)
 
     wn.update()
 
     while True:
-        print(f"Player {game.current_player}:")
+        print(f"\nPlayer {game.current_player}:")
 
         start_coords = list(map(int, input("\tStarting Coords: ").split()))
         end_coords = list(map(int, input("\tEnding Coords: ").split()))
-
-        if game.current_player == 2:
-            print()
 
         if game.check_move(start_coords, end_coords, game.current_player):
             game.make_turn(start_coords, end_coords)
---
+++ data/ui_components/turtle_functions.py
@@ -0,0 +1,85 @@
+import turtle
+
+
+def draw_hex(t):
+    for i in range(6):
+        t.forward(60)
+        t.left(60)
+
+
+def row_of_hexes(t, n):
+    for i in range(n-1):
+        draw_hex(t)
+        t.up()
+        t.forward(60)
+        t.right(60)
+        t.forward(60)
+        t.left(60)
+        t.down()
+    draw_hex(t)
+
+
+def snake(t, seq):
+    for c in seq:
+        if c == "f":
+            t.forward(60)
+        elif c == "r":
+            t.right(60)
+        else:
+            t.left(60)
+
+
+def draw_board(t, game):
+    t.speed(0)
+
+    t.up()
+    t.goto(-30, 365)
+    t.down()
+
+    row_of_hexes(t, 5)
+
+    snake(t, "flll")
+    row_of_hexes(t, 6)
+
+    snake(t, "flfrflflfl")
+    row_of_hexes(t, 7)
+
+    snake(t, "frrr")
+    row_of_hexes(t, 8)
+
+    snake(t, "flfrflflfl")
+    row_of_hexes(t, 9)
+
+    snake(t, "lllrfl")
+    row_of_hexes(t, 8)
+
+    snake(t, "flflfrflfl")
+    row_of_hexes(t, 7)
+
+    snake(t, "llfl")
+    row_of_hexes(t, 6)
+
+    snake(t, "flflfrflfl")
+    row_of_hexes(t, 5)
+
+
+def draw_pieces(wn, game):
+    with open("./resources/coordinate_data/hex_coords.txt") as f:
+        for line in f.readlines():
+            q, r, s, x, y = line.split()
+
+            a = turtle.Turtle()
+            a.up()
+            a.shape("circle")
+            a.goto(float(x), float(y))
+
+            key = f"{q}, {r}, {s}"
+            tile = game.board.board[key]
+            if tile.state == 0:
+                a.color("white")
+            elif tile.state == 1:
+                a.color("red")
+            else:
+                a.color("blue")
+
+    wn.update()
